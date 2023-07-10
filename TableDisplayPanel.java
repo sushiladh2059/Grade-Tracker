98f23923de6093a246f536261f536882aa81b392
Sushil Adhikari <sushiladh2059@gmail.com>
2023-07-10 08:29:49 +0545

    Everything worked, now need to change into socket programming

--- TableDisplayPanel.java
+++ TableDisplayPanel.java
@@ -14,38 +14,59 @@ public class TableDisplayPanel extends JPanel {
 
     private void createTablePanel(Double[] gpaData) {
         setLayout(new BorderLayout());
-
+    
         // Create the table model
-        DefaultTableModel tableModel = new DefaultTableModel(new Object[]{"Subject", "GPA"}, 0);
-
+        DefaultTableModel tableModel = new DefaultTableModel(new Object[]{"Subject", "GPA"}, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false; // Disable editing for all cells
+            }
+        };
+    
         // Add the data rows to the table model
         tableModel.addRow(new Object[]{"Maths", formatGrade(gpaData[0])});
         tableModel.addRow(new Object[]{"English", formatGrade(gpaData[1])});
         tableModel.addRow(new Object[]{"Web", formatGrade(gpaData[2])});
         tableModel.addRow(new Object[]{"Logic", formatGrade(gpaData[3])});
-
+    
         // Calculate the average GPA
         double averageGPA = calculateAverageGPA(gpaData);
-
+    
         // Add the average GPA row to the table model
         tableModel.addRow(new Object[]{"Average GPA", formatGrade(averageGPA)});
-
+    
         // Create the table
         JTable table = new JTable(tableModel);
         table.setShowGrid(true);
-        table.setGridColor(Color.GRAY);
-
+    
         // Center align the cells in the table
         DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
         centerRenderer.setHorizontalAlignment(JLabel.CENTER);
         table.setDefaultRenderer(Object.class, centerRenderer);
-
+    
+        // Set custom cell renderer for the "Average GPA" row
+        table.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+    
+                // Check if it is the "Average GPA" row
+                if (table.getValueAt(row, 0).equals("Average GPA")) {
+                    component.setBackground(Color.LIGHT_GRAY);
+                } else {
+                    component.setBackground(Color.WHITE);
+                }
+    
+                return component;
+            }
+        });
+    
         // Create a scroll pane for the table
         JScrollPane scrollPane = new JScrollPane(table);
-
+    
         // Add the scroll pane to this panel
         add(scrollPane, BorderLayout.CENTER);
-
+    
         // Create the back button
         JButton backButton = new JButton("BACK");
         backButton.addActionListener(e -> {
@@ -53,14 +74,16 @@ public class TableDisplayPanel extends JPanel {
             CardLayout cardLayout = (CardLayout) parentPanel.getLayout();
             cardLayout.show(parentPanel, "inputPanel");
         });
-
-        // Create a panel for the back button
+    
+        // Create a panel for the buttons
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         buttonPanel.add(backButton);
-
+    
         // Add the button panel to the bottom of the panel
         add(buttonPanel, BorderLayout.SOUTH);
     }
+    
+    
 
     private double calculateAverageGPA(Double[] gpaData) {
         double sum = 0.0;
